108c2ac8ca8026b44d072851351ed65b9cb69222
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-21 19:24:45 -0300

    Create User.java

---
+++ stateful/stateful-auth-api/src/main/java/br/com/microservice/statefulauthapi/core/model/User.java
@@ -0,0 +1,24 @@
+package br.com.microservice.statefulauthapi.core.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user", schema = "public")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+}
