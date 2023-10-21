1cb5bf72c3a2357479ebc3919fb9464d7c7ccfa0
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-21 19:49:41 -0300

    create - ExceptionHandler

---
+++ stateful/stateful-auth-api/src/main/java/br/com/microservice/statefulauthapi/infra/exception/ExceptionDetails.java
@@ -0,0 +1,4 @@
+package br.com.microservice.statefulauthapi.infra.exception;
+
+public record ExceptionDetails(int status, String message) {
+}
---
+++ stateful/stateful-auth-api/src/main/java/br/com/microservice/statefulauthapi/infra/exception/ExceptionGlobalHandler.java
@@ -0,0 +1,24 @@
+package br.com.microservice.statefulauthapi.infra.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionGlobalHandler {
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<?> handleValidationException(ValidationException exception) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ExceptionDetails details = new ExceptionDetails(status.value(), exception.getMessage());
+        return new ResponseEntity<>(details, status);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<?> handleAuthenticationException(AuthenticationException exception) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        ExceptionDetails details = new ExceptionDetails(status.value(), exception.getMessage());
+        return new ResponseEntity<>(details, status);
+    }
+}
