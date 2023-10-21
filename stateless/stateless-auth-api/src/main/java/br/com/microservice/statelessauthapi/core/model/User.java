7ae5a212d64e7ec59917b90db6298c69656fea83
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-21 19:14:35 -0300

    Create User.java

---
+++ stateless/stateless-auth-api/src/main/java/br/com/microservice/statelessauthapi/core/model/User.java
@@ -0,0 +1,27 @@
+package br.com.microservice.statelessauthapi.core.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user", schema = "public")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+
+}
+
+
