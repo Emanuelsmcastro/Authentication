f6d0d61cda90ca3cdbaf3f6315361b7dc0504d5f
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-22 08:35:02 -0300

    Create AuthService.java

---
+++ stateless/stateless-auth-api/src/main/java/br/com/microservice/statelessauthapi/core/model/service/AuthService.java
@@ -0,0 +1,45 @@
+package br.com.microservice.statelessauthapi.core.model.service;
+
+import br.com.microservice.statelessauthapi.core.model.User;
+import br.com.microservice.statelessauthapi.core.model.dto.AuthRequest;
+import br.com.microservice.statelessauthapi.core.model.dto.TokenDTO;
+import br.com.microservice.statelessauthapi.core.repository.UserRepository;
+import br.com.microservice.statelessauthapi.infra.exception.ValidationException;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    public TokenDTO login(AuthRequest request) {
+        User user = userRepository.findByUsername(request.username())
+                .orElseThrow(() -> new ValidationException("User not found."));
+        String accessToken = jwtService.createToken(user);
+        validatePassword(request.password(), user.getPassword());
+        return new TokenDTO(accessToken);
+    }
+
+    public TokenDTO validateToken(String accessToken) {
+        validateExistingToken(accessToken);
+        jwtService.validateAccessToken(accessToken);
+        return new TokenDTO(accessToken);
+    }
+
+    private void validatePassword(String rawPassword, String encodedPassword) {
+        if (!passwordEncoder.matches(rawPassword, encodedPassword))
+            throw new ValidationException("Incorrect password.");
+    }
+
+    private void validateExistingToken(String accessToken) {
+        if (ObjectUtils.isEmpty(accessToken))
+            throw new ValidationException("Token not found.");
+    }
+
+}
