c1d35f9e4d42e28d5a623170665423529a65793a
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-22 18:27:16 -0300

    Update TokenClient.java

--- stateful/stateful-any-api/src/main/java/br/com/microservice/statefulanyapi/core/client/TokenClient.java
+++ stateful/stateful-any-api/src/main/java/br/com/microservice/statefulanyapi/core/client/TokenClient.java
@@ -1,4 +1,18 @@
 package br.com.microservice.statefulanyapi.core.client;
 
+import br.com.microservice.statefulanyapi.core.dto.AuthUserResponse;
+import br.com.microservice.statefulanyapi.core.dto.TokenDTO;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PostExchange;
+
+@HttpExchange(value = "/api/auth")
 public interface TokenClient {
+
+    @PostExchange("/token/validate")
+    TokenDTO validateToken(@RequestHeader String accessToken);
+
+    @GetExchange("/user")
+    public AuthUserResponse getAuthUser(@RequestHeader String accessToken);
 }
