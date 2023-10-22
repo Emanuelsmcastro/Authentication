5c566496c08fa6a881a65fa5dfad16b4216b0918
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-22 16:42:17 -0300

    Create UserRepository.java

---
+++ stateful/stateful-auth-api/src/main/java/br/com/microservice/statefulauthapi/core/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.microservice.statefulauthapi.core.repository;
+
+import br.com.microservice.statefulauthapi.core.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+}
